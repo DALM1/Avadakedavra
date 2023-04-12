47dc39c5b16760549998e214ac3d0925047eef99
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-04-12 18:52:52 +0200

    Updates

--- stringparser.sh
+++ stringparser.sh
@@ -5,7 +5,9 @@ dir="/c/Users/User"
 
 find "$dir" -type f -print0 | while IFS= read -r -d '' file; do
     echo "Nom de fichier : $file"
+    echo "====================="
     echo "Contenu de $file :"
+    echo "====================="
     cat "$file"
     echo "====================="
 done
